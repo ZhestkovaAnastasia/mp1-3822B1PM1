003c23d84e9ecca8219cc743f7368197a31c4822
ana.steisha <119804721+ZhestkovaAnastasia@users.noreply.github.com>
2023-06-08 23:37:41 +0300

    Add files via upload

---
+++ Jordan_Gauss/Jordan_Gauss/Jordan_Gauss.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <clocale>
+#include "Matrix.h"
+#include "Solver.h"
+#include "Vector.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    size_t n;
+    cout << "Введите размерность вектора и квадратной матрицы: ";
+    cin >> n;
+
+    int a;
+    cout << "Выберите тип данных:\n 1. float\n 2. double\n";
+    cin >> a;
+
+    switch (a) {
+    case 1: {
+        solver<float>(n);
+        break; }
+    case 2: {
+        solver<double>(n);
+        break; }
+    }
+    return 0;
+}
---
+++ Jordan_Gauss/Jordan_Gauss/Matrix.h
@@ -0,0 +1,90 @@
+#pragma once
+#include<iostream>
+#include "Vector.h"
+
+using namespace std;
+
+template <typename T>
+class Matrix {
+    T** mas;
+    size_t size;
+public:
+    Matrix(size_t size)
+    {
+        this->size = size;
+        mas = new T * [size];
+
+        for (int i = 0; i < size; i++)
+            mas[i] = new T[size + 1];
+    }
+
+    Matrix(const Matrix<T>& m)
+    {
+        size = m.size;
+        mas = new T * [size];
+
+        for (int i = 0; i < size; i++)
+            mas[i] = new T[size + 1];
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size + 1; j++)
+                mas[i][j] = m.mas[i][j];
+    }
+
+
+    void Set_Matrix()
+    {
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                cin >> mas[i][j];
+    }
+
+    T Get_Matrix(int i, int j)
+    {
+        return mas[i][j];
+    }
+
+    void extended_matrix(Vector<T> b)  //создание расширенной матрицы
+    {
+        for (int i = 0; i < size; i++)
+            mas[i][size] = b.Get_Vector(i);
+    }
+
+    void new_value_matrix(int i, int j, T new_val) //присваивание нового значения элементу матрицы new value
+    {
+        mas[i][j] = new_val;
+    }
+
+    void swap_elem(int i1, int i2)
+    {
+        T w;
+        for (int j = 0; j < size + 1; j++)
+        {
+            w = mas[i1][j];
+            mas[i1][j] = mas[i2][j];
+            mas[i2][j] = w;
+        }
+    }
+
+    void print_matrix()
+    {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++)
+            {
+                cout <<  mas[i][j] << " ";
+            }
+            cout << "|" << mas[i][size] << "\n";
+        }
+        cout << "\n";
+    }
+
+    ~Matrix()
+    {
+        if (size > 0) {
+            for (int i = 0; i < size; i++)
+                delete[] mas[i];
+            delete[] mas;
+            size = 0;
+        }
+    }
+};
